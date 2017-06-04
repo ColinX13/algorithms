4bd26e260d6066fabeb4d6886400621764559608
Colin Xie <colinxie99@gmail.com>
2017-06-04 11:52:54 -0700

    Fixed comments in Solution #60

--- solutions/60.js
+++ solutions/60.js
@@ -1,13 +1,12 @@
-//Return the sum of all factors in x
-//Intialize sum to equal 0
-//Factor i of number have mod equal to 0
-//Reassign sum to equal to sum plus i
-//Run through for loop
+//Sum of its factors
+//Write a function that given a number returns the sum of all of its factors.
 //input [2,4,10,24]
 //output [3,7,18,60]
 
 // Solution by Colin Xie @ColinX13
 
+// @param {number[]} x - A number input
+// @returns {number[]} sum - The sum of all the factors of a number
 const solution = (x) => {
   let sum = 0
   for(let i = x; i >= 1; i--){
