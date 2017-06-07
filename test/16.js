eef599d7957604c05da217aed579c150221f1c0e
Colin Xie <colinxie99@gmail.com>
2017-06-07 16:47:07 -0700

    #16 solutions and test

--- test/16.js
+++ test/16.js
@@ -1,17 +1,17 @@
 const expect = require('chai').expect;
 let solution = require('../solutions/16').solution;
-// solution = require('./yourSolution').solution;
+solution = require('../yourSolution').solution;
 
 describe('check duplicates', () => {
-  it('should return true since array has duplicates', () => {
+  it('should return true since array has duplicates [1,2,3,3,4] ', () => {
     const array = [1,2,3,3,4];
     expect(solution(array)).to.be.true;
   });
-  it('should return false because array does not have duplicates', () => {
+  it('should return false because array does not have duplicates [1,2,3,4]', () => {
     const array = [1,2,3,4];
     expect(solution(array)).to.be.false;
   });
-  it('should return false because array is empty', () => {
+  it('should return false because array is emptyi []', () => {
     const array = [];
     expect(solution(array)).to.be.false;
   });
--- yourSolution.js
+++ yourSolution.js
@@ -1,7 +1,22 @@
 // Name
 // Problem Description
 
-const solution = () => {
+const solution = (arr) => {
+  let obj = {};
+  for(let i = 0; i < arr.length; i++){
+    if(!obj[arr[i]]){
+      obj[arr[i]] = 1;
+    }
+    else{
+      obj[arr[i]]++;
+    }
+  }
+  for(let key in obj){
+    if(obj[key] > 1){
+      return true;
+    }
+  }
+  return false;
 };
 
 module.exports = {
