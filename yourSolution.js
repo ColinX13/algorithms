66ab3ad4edbc78ee28dc877f1078f37c31cac91e
Colin Xie <colinxie99@gmail.com>
2017-06-09 09:29:31 -0700

    solutions #8

--- yourSolution.js
+++ yourSolution.js
@@ -1,7 +1,13 @@
 // Name
-// Problem Description
+// Take a string and reverse the spelling
 
-const solution = () => {
+const solution = (str) => {
+  let newStr = "";
+  for(let i = str.length - 1; i > -1; i--){
+    newStr = newStr + str[i];
+  }
+  //  console.log(newStr);
+  return newStr;
 };
 
 module.exports = {
